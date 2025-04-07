a1b7e79b31b7931022c576a725b35583d1907e26
csp0017 <barrabinoguachupino@gmail.com>
2025-04-07 23:50:11 +0200

    subo conexion y buscar_usuarios y vivienda 07/04 23:50

--- utiles/conexion.php
+++ utiles/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $_servidor = "sql302.infinityfree.com"; // O "localhost"
+    $_servidor = "sql302.byetcluster.com"; // O "localhost"
     $_usuario = "if0_38638851";
     $_contrasena = "4IbtiTJdujdk";
     $_base_de_datos = "if0_38638851_compiso_db";
